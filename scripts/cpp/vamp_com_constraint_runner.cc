bdfce56086b5a3dff7111673454d5b4dc15fbf4b
ShrutheeshIR <shrutheesh99@gmail.com>
2026-01-08 16:26:03 -0500

    add manual print

--- scripts/cpp/vamp_com_constraint_runner.cc
+++ scripts/cpp/vamp_com_constraint_runner.cc
@@ -30,20 +30,6 @@ static constexpr Robot::ConfigurationArray goal = {0.035,0.0,0.031,0.0,0.0,0.0,0
 // static constexpr Robot::ConfigurationArray goal = {-1.62706,-0.100903,2.59477,-2.09287,1.2912,1.8256,-0.7243,1.75215,1.5907,-2.0261,-1.8123,-0.119768,2.50718,-0.7243};
 
 
-struct Attempt {
-    float range;
-    bool dynamic_domain;
-    vamp::planning::ProjMethod proj_method;
-    float descend_rate;
-    bool success;
-    std::size_t planning_time;
-    std::size_t planning_iterations;
-    std::size_t path_length;
-
-    bool operator<(const Attempt& other) const {
-        return planning_time < other.planning_time;
-    }
-};
 
 
 auto main(int, char **) -> int
@@ -51,27 +37,6 @@ auto main(int, char **) -> int
 
     vamp::planning::RRTCSettings rrtc_settings;
 
-    float ranges[] = {0.5, 0.75, 1.0, 1.5, 2.0};
-    // float ranges[] = {1.0};
-    // float ranges[] = {0.1, 0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 2.5, 3.0};
-    // bool dd[] = {false};
-    bool dd[] = {false, true};
-    vamp::planning::ProjMethod projection_method[] = {vamp::planning::ProjMethod::InnerLM, vamp::planning::ProjMethod::OuterLM, vamp::planning::ProjMethod::GradDesc};
-    // vamp::planning::ProjMethod projection_method[] = {vamp::planning::ProjMethod::InnerLM, vamp::planning::ProjMethod::OuterLM};
-    // vamp::planning::ProjMethod projection_method[] = {vamp::planning::ProjMethod::InnerLM};
-
-    float descend_rates[] = {0.25, 0.5, 0.75, 1.0};
-    // float descend_rates[] = {0.75, 1.0};
-    // float descend_rates[] = {1.0};
-    std::vector<Attempt> succ_attempts;
-    for(const auto range: ranges){
-        for(const auto dyndom: dd){
-            for(const auto &pm: projection_method){
-                for(const auto descent_rate: descend_rates){
-
-                // if(pm < 2) continue;
-
-    // Build sphere cage environment
     EnvironmentInput environment;
     // std::ofstream outfile_sph("spheres.txt");
     // for (const auto &sphere : problem)
@@ -189,36 +154,18 @@ auto main(int, char **) -> int
     //     com_constraint
     // );
 
-    rrtc_settings.range = range;
+    rrtc_settings.range = 1.0;
     rrtc_settings.max_iterations = 100000;
-    rrtc_settings.dynamic_domain = dyndom;
-    rrtc_settings.projection_method = pm;
-    rrtc_settings.descend_rate = descent_rate;
-    // std::cout << "\n\n-----------------Starting to cbirrt------------ " << std::endl;
-    std::cout << range << ", " << dyndom << " " << pm << " " << descent_rate << " ";
+    rrtc_settings.dynamic_domain = true;
+    rrtc_settings.projection_method = vamp::planning::ProjMethod::InnerLM;
+    rrtc_settings.descend_rate = 1.0;
     auto result =
         vamp::planning::CRRTC<Robot, rake, Robot::resolution, decltype(feet_tsr_constraint), decltype(com_constraint), decltype(bimanual_constraint)>::solve(Robot::Configuration(start), Robot::Configuration(goal), env_v, rrtc_settings, task_constraint, rng);
-    // auto result =
-    //     vamp::planning::CRRTC<Robot, rake, Robot::resolution, decltype(bimanual_constraint), decltype(com_constraint)>::solve(Robot::Configuration(start), Robot::Configuration(goal), env_v, rrtc_settings, task_constraint, rng);
 
     if(result.path.size() > 0)
     {
-        Attempt a{
-            range,
-            dyndom,
-            pm,
-            descent_rate,
-            true,
-            result.nanoseconds,
-            result.iterations,
-            result.path.size()
-        };
-
-
-        if((succ_attempts.size() == 0) || (succ_attempts.size() > 0 && a < succ_attempts[0])){
 
         std::cout << "\nPrinting Result!! " << result.path.size() << std::endl;
-        // Output configurations of simplified path
         std::cout << std::fixed << std::setprecision(3);
         std::ofstream outfile("/src/trajectory.txt");
         for (const auto &config : result.path)
@@ -228,7 +175,6 @@ auto main(int, char **) -> int
             bool first = true;
             for (auto i = 0U; i < Robot::dimension; ++i)
             {
-                // std::cout << array[i] << ", ";
                 soln[i] = array[i];
 
                 if (!first) outfile << ",";
@@ -236,33 +182,9 @@ auto main(int, char **) -> int
                 first = false;
             }
 
-            // auto fka = Robot::eefk(soln);
-            // std::cout <<std::endl << fka.matrix() <<std::endl;
-            // std::cout << std::endl;
             outfile << "\n";
         }
         outfile.close();
     }
-        // std::cin.ignore();
-        succ_attempts.push_back(a);
-        std::sort(succ_attempts.begin(), succ_attempts.end());
-
-
-
-    }
-            }
-        }
-    }
-    }
-    std::cout << "------Final Result --------" << std::endl;
-    std::sort(succ_attempts.rbegin(), succ_attempts.rend());
-    for (const auto &a : succ_attempts) {
-        std::cout << a.range << ", " << a.dynamic_domain << ", " << a.proj_method << ", " << a.descend_rate << ", " << a.planning_time/1e6 << ", " << a.planning_iterations << ", " << a.path_length << std::endl;
-
-    }
-    std::cout << "---------------------------" << std::endl;
-
-
-
     return 0;
 }
