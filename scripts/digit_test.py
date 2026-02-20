b319d1caefe9264d477cf24d624e2cccbe16211e
ShrutheeshIR <shrutheesh99@gmail.com>
2026-02-20 13:36:04 -0500

    real world test

--- scripts/digit_test.py
+++ scripts/digit_test.py
@@ -4,11 +4,81 @@ from pathlib import Path
 import json
 import vamp
 from fire import Fire
-from scipy.spatial.transform import Rotation as R
 import itertools
 import meshcat_viz as viz
+from scipy.spatial.transform import Rotation as R
 
 import mjcf_parser
+
+
+start = [
+    0.0287480000000000, -0.0189052000000000, -0.0145299000000000, 0.00967345000000000, 0.00825856000000000, -0.00215209000000000, 
+    0.397209000000000, -0.00820680000000000, 0.284002000000000, 0.290042000000000, -0.0213535000000000, -0.235481000000000, -0.0319210000000000, 0.00127043000000000,
+    -0.0782953000000000, 1.04159000000000, 0.0284093000000000, -0.0123198000000000,
+    -0.358284000000000, -0.0133610000000000, -0.278441000000000, -0.268447000000000, 0.0177607000000000, 0.212721000000000, -0.0692397000000000, -0.000343739000000000,
+    0.0689620000000000, -1.23829000000000, 0.0369306000000000, -0.00862385000000000
+]
+
+box_top_shelf_pickup = [
+    1.11889839e-02, -1.99530125e-02,  5.81884384e-03, -3.29416106e-03      ,                                                                                                                   
+ -1.42211439e-02, -5.99248195e-03,  3.84082675e-01,  5.58406231e-04        ,                                                                                                                 
+  2.98647016e-01,  3.22717577e-01, -3.98048153e-03, -3.00990015e-01        ,                                                                                                                 
+  1.07899308e-02,  1.02880923e-02,  3.03654131e-02,  1.43816188e-01        ,                                                                                                                 
+ -3.00536864e-04, -5.22131145e-01, -3.72209966e-01, -3.30008916e-03        ,                                                                                                                 
+ -2.98724055e-01, -3.18776041e-01,  8.88650957e-03,  2.83709198e-01        ,                                                                                                                 
+ -1.18316561e-01,  6.75600534e-03,  4.73398268e-02, -1.23632416e-01        ,                                                                                                                 
+  2.74537895e-02,  4.99689251e-01
+]
+
+rack_2 = [
+    0.01814485, -0.01102245, -0.4702971 , -0.01578958, -0.02233502, -0.00822024,                                                                                                                 
+  0.40243718,  0.00760839, -0.2222104 , -0.8473681 , -0.01940396,  0.89499557  ,                                                                                                               
+ -0.5288469 ,  0.04388458, -0.01270674,  0.3497003 ,  0.09755287,  0.8987735   ,                                                                                                               
+ -0.35999396, -0.00482713,  0.21748814,  0.8451087 ,  0.02017486, -0.90229964  ,                                                                                                               
+  0.41662216,  0.03758987, -0.15651442, -0.29156512, -0.1713613 , -0.96002305,
+]
+
+rack_3 = [
+    0.00927,-0.01219,-0.47283,-0.01411,-0.02955,-0.00937,0.40005,0.01408,-0.23717,-0.84704,-0.02429,0.90190,-0.48186,0.04543,-0.17059,-0.36363,0.06204,1.09005,-0.36164,-0.00267,0.22962,0.84538,0.02448,-0.90900,0.40052,0.03680,-0.29646,0.37895,-0.15478,-1.16821
+]
+
+
+def convert_trajectory_eef_to_controller_format(trajectory, eef_waypoints):
+    # create a 8 + 7 + 7 + 7 = 29 dim waypoint
+    # the first 8 values are left and right arms joint positions
+    # next 7 is the floating base pose (x, y, z, qw, qx, qy, qz)
+    # next 7 is left foot pose (x, y, z, qw, qx, qy, qz)
+    # next 7 is right foot pose (x, y, z, qw, qx, qy, qz)
+    # eef_waypoints is a n x 4 x 4 x 4 array where the first 4 is number of eefs, n is number of waypoints, and the last 4x4 is the transform of the eef in world frame
+
+    print(eef_waypoints.shape)
+
+    output = np.zeros((len(trajectory), 29))
+    # arm joints are 14:18 and, 26:29
+    output[:, :8] = trajectory[:, [14, 15, 16, 17, 26, 27, 28, 29]]
+    # floating base is 0:6. Need to convert euler to quat for orientation
+
+    def to_quat(euler):
+        r = R.from_euler('xyz', euler, degrees=False)
+        return r.as_quat() # returns in (x, y, z, w) format
+
+    def to_quat_from_rotmatrix(rotmat):
+        r = R.from_matrix(rotmat)
+        return r.as_quat() # returns in (x, y, z, w) format
+
+    output[:, 8:11] = trajectory[:, 0:3]
+    output[:, 11:15] = np.apply_along_axis(to_quat, 1, trajectory[:, 3:6])
+    # left foot is eef_waypoints[:, 2] as a 4x4 matrix
+    # need to convert it to (x, y, z, qw, qx, qy, qz) format
+    output[:, 15:18] = eef_waypoints[:, 2, :3, 3] # position
+    output[:, 18:22] = np.array([to_quat_from_rotmatrix(eef_waypoints[i, 2, :3, :3]) for i in range(len(eef_waypoints))]) # orientation
+    # right foot is eef_waypoints[:, 3] as a 4x4 matrix
+    output[:, 22:25] = eef_waypoints[:, 3, :3, 3] # position
+    output[:, 25:29] = np.array([to_quat_from_rotmatrix(eef_waypoints[i, 3, :3, :3]) for i in range(len(eef_waypoints))]) # orientation
+
+    return output
+
+
 
 
 def get_eef_of_waypoints(waypoints):
@@ -17,44 +87,24 @@ def get_eef_of_waypoints(waypoints):
     eef_poses = []
     for waypoint in waypoints:
         eef_pose = [pose.tolist() for pose in vamp_module.eefk(waypoint)]
-        eef_poses.extend(eef_pose)
-    return eef_poses
+        eef_poses.append(eef_pose)
+    return np.array(eef_poses)
 
 def run_planner(
+    start,
+    goal,
     combination,
+    constraint = 0,
     obstacle_radius: float = 0.15,
     attachment_radius: float = 0.05,
     attachment_offset: float = 0.02,
     **kwargs,
 ):
 
-    start = [
-        0.0287480000000000, -0.0189052000000000, -0.0145299000000000, 0.00967345000000000, 0.00825856000000000, -0.00215209000000000, 
-        0.397209000000000, -0.00820680000000000, 0.284002000000000, 0.290042000000000, -0.0213535000000000, -0.235481000000000, -0.0319210000000000, 0.00127043000000000,
-        -0.0782953000000000, 1.04159000000000, 0.0284093000000000, -0.0123198000000000,
-        -0.358284000000000, -0.0133610000000000, -0.278441000000000, -0.268447000000000, 0.0177607000000000, 0.212721000000000, -0.0692397000000000, -0.000343739000000000,
-        0.0689620000000000, -1.23829000000000, 0.0369306000000000, -0.00862385000000000
-    ]
-
-    box_top_shelf_pickup = [
-        0.00799012, -0.02020776,  0.00473523, -0.00283502, -0.01516565, -0.00604922,                                                                                                                                                                                                             
-        0.38425133,  0.0008674,   0.29416478,  0.32168943, -0.00645173, -0.296634,                                                                                                                                                                                                               
-        0.02625096,  0.01125011,  0.0887334,  -0.26175958, -0.00802566,  0.11402924,                                                                                                                                                                                                             
-        -0.37266037, -0.00365927, -0.2966428,  -0.3174328,   0.01091318,  0.27964896,                                                                                                                                                                                                             
-        -0.13281101,  0.00645007,  0.16188946,  0.27721465,  0.06585242, -0.09529761,
-    ]
-
-    rack_2 = [
-        0.00927,-0.01219,-0.47283,-0.01411,-0.02955,-0.00937,0.40005,0.01408,-0.23717,-0.84704,-0.02429,0.90190,-0.48186,0.04543,-0.17059,-0.36363,0.06204,1.09005,-0.36164,-0.00267,0.22962,0.84538,0.02448,-0.90900,0.40052,0.03680,-0.29646,0.37895,-0.15478,-1.16821
-    ]
-
-    rack_3 = [
-        0.00927,-0.01219,-0.47283,-0.01411,-0.02955,-0.00937,0.40005,0.01408,-0.23717,-0.84704,-0.02429,0.90190,-0.48186,0.04543,-0.17059,-0.36363,0.06204,1.09005,-0.36164,-0.00267,0.22962,0.84538,0.02448,-0.90900,0.40052,0.03680,-0.29646,0.37895,-0.15478,-1.16821
-    ]
 
 
     polygon_points = [
-        0.01, -0.01, 0.01, 0.03, -0.03, 0.03, -0.03, -0.01
+        0.005, -0.01, 0.005, 0.03, -0.05, 0.03, -0.05, -0.01
     ]
 
     bimanual_limit_lower_bound = [
@@ -67,15 +117,15 @@ def run_planner(
     tsr_lower_bound = [
         -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, 
         -10.0, -10.0, -10.0, -10.0, -10.0, -10.0, 
-        -0.001, -0.001, -0.001, -0.1, -0.1, -0.1, 
-        -0.001, -0.001, -0.001, -0.1, -0.1, -0.1
+        -0.001, -0.001, -0.001, -0.03, -0.03, -0.03, 
+        -0.001, -0.001, -0.001, -0.03, -0.03, -0.03
     ]
 
     tsr_upper_bound = [
         10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 
         10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 
-        0.001, 0.001, 0.001, 0.1, 0.1, 0.1, 
-        0.001, 0.001, 0.001, 0.1, 0.1, 0.1
+        0.001, 0.001, 0.001, 0.03, 0.03, 0.03, 
+        0.001, 0.001, 0.001, 0.03, 0.03, 0.03
     ]
 
 
@@ -120,6 +170,7 @@ def run_planner(
 
 
 
+
     e = vamp.Environment()
     env_geoms = mjcf_parser.parse_mjcf('resources/environments/cuboids/wooden_shelf.xml')
     for geom in env_geoms:
@@ -146,11 +197,11 @@ def run_planner(
         [0.04, 0.01, -0.26],
     ]
 
-    attachment.add_spheres(
-        [
-            vamp.Sphere(sphere, 0.05) for sphere in attach_spheres
-        ]
-    )
+    # attachment.add_spheres(
+    #     [
+    #         vamp.Sphere(sphere, 0.05) for sphere in attach_spheres
+    #     ]
+    # )
     e.attach(attachment, 0);
 
     sampler = vamp_module.halton()
@@ -165,11 +216,20 @@ def run_planner(
     plan_settings.num_projection_iterations = 10;
     plan_settings.insert_all_to_tree = True;
 
-    # result = planner_func(start, box_top_shelf_pickup, e, plan_settings, non_transport_constraints, sampler)
-    result = planner_func(box_top_shelf_pickup, rack_2, e, plan_settings, transport_constraints, sampler)
+    if constraint == 0:
+        task_constraint = non_transport_constraints
+    else:
+        task_constraint = transport_constraints
 
-    simple = vamp_module.simplify_with_constraints(result.path, e, transport_constraints, simp_settings, sampler)
-    # simple = vamp_module.simplify_with_constraints(result.path, e, non_transport_constraints, simp_settings, sampler)
+    print(task_constraint.distanceToConstraint(np.array(start)))
+    print(task_constraint.distanceToConstraint(np.array(goal)))
+
+    print(task_constraint.projectConfiguration(np.array(start), 0, 10.0, 0.75, 50, True))
+    print(task_constraint.projectConfiguration(np.array(goal), 0, 10.0, 0.75, 50, True))
+
+
+    result = planner_func(start, goal, e, plan_settings, task_constraint, sampler)
+    simple = vamp_module.simplify_with_constraints(result.path, e, task_constraint, simp_settings, sampler)
 
     return result, simple
 
@@ -178,7 +238,7 @@ if __name__ == '__main__':
     # ranges = [0.5, 0.75, 1.0, 1.5]
     # dyndoms = [False, True]
     ranges = [0.75, 0.5, 1.0]
-    dyndoms = [True, False]
+    dyndoms = [False]
 
     all_combinations = list(itertools.product(ranges, dyndoms))
 
@@ -188,7 +248,7 @@ if __name__ == '__main__':
     for _ in range(1):
         for combination in all_combinations:
             print("Running combination ", combination)
-            result, simple = run_planner(combination)
+            result, simple = run_planner(start, box_top_shelf_pickup, combination, constraint = (start == box_top_shelf_pickup))
             planning_times[combination].append(result.nanoseconds/1e6)
     print("Execution completed")
     print("Planning times for each combination:")
@@ -209,17 +269,48 @@ if __name__ == '__main__':
     viz.clear_all_waypoints()
 
 
-    result, simple = run_planner(best_combination[0])
-    print(result.nanoseconds/1e6)
+    result1, simple1 = run_planner(start, box_top_shelf_pickup, best_combination[0], constraint = 0)
+    simple1.path.interpolate_to_resolution(vamp.digit.resolution())
+    traj1 = simple1.path.numpy()
 
-    if len(simple.path.numpy()):
-        simple.path.interpolate_to_resolution(vamp.digit.resolution())
+    result2, simple2 = run_planner(box_top_shelf_pickup, rack_2, best_combination[0], constraint = 1)
+    simple2.path.interpolate_to_resolution(vamp.digit.resolution())
+    traj2 = simple2.path.numpy()
 
-        traj = simple.path.numpy()
-        eef_poses = get_eef_of_waypoints(traj)
-        
-        viz.render_eefs(np.array(eef_poses))
-        viz.animate(traj, np.arange(0, len(traj), dtype=np.float64) / 10, loop=True)
+    # now combine the trajectories
+
+    final_traj = np.concatenate((traj1, traj2), axis=0)
+    # print(final_traj.shape)
+    # stop
+
+    # final_traj = traj1
+
+
+    full_waypoints = np.array(final_traj)
+    waypoints_no_floating = full_waypoints[:, 6:]
+    final_waypoints = np.zeros((len(waypoints_no_floating), 30))
+    final_waypoints[:, :6] = waypoints_no_floating[:, :6]
+    final_waypoints[:, 9:21] = waypoints_no_floating[:, 6:18]
+    final_waypoints[:, 24:30] = waypoints_no_floating[:, 18:24]
+    np.savetxt('/src/vamp/shelf_top_to_down.txt', final_waypoints, fmt='%.4f', delimiter = ',')
+
+    print("Final time: ", result1.nanoseconds/1e6 + result2.nanoseconds/1e6)
+
+
+    eef_poses = get_eef_of_waypoints(final_traj)
+    controller_task_space_traj = convert_trajectory_eef_to_controller_format(final_traj, eef_poses)
+    np.savetxt('/src/vamp/shelf_top_to_down_controller_format.txt', controller_task_space_traj, fmt='%.4f', delimiter = ',')
+
+
+    if len(final_traj) > 0:
+        # simple.path.interpolate_to_resolution(vamp.digit.resolution())
+
+        # traj = simple.path.numpy()
+        eef_poses = get_eef_of_waypoints(final_traj)
+
+        # for viz flatten the first two dimensions so it's just a list of eef poses        
+        viz.render_eefs(eef_poses.reshape(-1, 4, 4))
+        viz.animate(final_traj, np.arange(0, len(final_traj), dtype=np.float64) / 10, loop=True)
 
 
         while True:
